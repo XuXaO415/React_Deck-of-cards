fae706d5e137032a8d10a09d61dc281f38298f5f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-17 18:19:00 -0700

    Added setup for this project

---
+++ src/Deck.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Card from "./Card";
+
+const Base_URL= 'https://deckofcardsapi.com/api/deck'
+const Deck = () => {
+
+}
+
+export default Deck;
