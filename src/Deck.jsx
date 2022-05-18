53b8401ab915e00e5dfb4b93b375725a3f7344fa
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-17 20:56:29 -0700

    Added useEffect

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -3,12 +3,21 @@ import axios from "axios";
 import Card from "./Card";
 
 
-const Base_URL= 'https://deckofcardsapi.com/api/deck'
-const Deck = () => {
-    const [deck, setDeck] = useState(null)
-    draw 
-    shuffle 
+const Base_URL= 'https://deckofcardsapi.com/api/deck';
 
+const Deck = () => {
+    const [deck, setDeck] = useState(null);
+    const[draw, setDraw] = useState([]);
+    const[newCard, setNewCard] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            const apiRes = await axios.get(`${Base_URL}/new/shuffle`)
+            setDeck(apiRes.data)
+        }
+        fetchData();
+    }, [setDeck]);
+ 
 
 
 }
