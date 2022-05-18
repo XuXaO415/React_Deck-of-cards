f969e5984de21d3116a402aa091337c88e78ce98
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-17 20:42:46 -0700

    Currently working on this project

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,6 +2,10 @@ import React, {useState} from "react";
 
 const Card = () => {
 
+    return (
+        
+    )
+
 }
 
 export default Card;
--- src/Deck.jsx
+++ src/Deck.jsx
@@ -1,9 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import axios from "axios";
 import Card from "./Card";
 
+
 const Base_URL= 'https://deckofcardsapi.com/api/deck'
 const Deck = () => {
+    const [deck, setDeck] = useState(null)
+    draw 
+    shuffle 
+
+
 
 }
 
