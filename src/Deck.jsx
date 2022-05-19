62a8643d7d81076b4074c4f4743bdc7657cfb3c1
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-18 20:30:33 -0700

    Completed this exercise

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -1,18 +1,18 @@
 import React, {useState, useEffect, useRef} from "react";
 import axios from "axios";
 import Card from "./Card";
-import "./Card"
+import "./Card.css"
 
 
-const Base_URL= 'https://deckofcardsapi.com/api/deck';
+const Base_URL= 'https://deckofcardsapi.com/api/deck/';
 
 
 const Deck = () => {
     const [deck, setDeck] = useState(null);
     const[draw, setDraw] = useState([]);
-    const[newCard, setNewCard] = useState(true);
+    // const[newCard, setNewCard] = useState(0);
     const timerId  = useRef(null);
-    const [card, setCard] = useState(0);
+    const [card, setCard] = useState();
     const [autoDraw, setAutoDraw] = useState(false);
 
     useEffect(() => {
@@ -23,13 +23,14 @@ const Deck = () => {
         fetchData();
     }, [setDeck]);
 
-    useEffect(() => {
+    useEffect(function renderDrawnCard() {
         async function drawCard() {
             let deck_id = deck;
             const drawRes = await axios.get(`${Base_URL}/${deck_id}/draw/`);
             if(drawRes.data.remaining === 0) {
-                setNewCard(false);
+                setAutoDraw(false);
                 alert("Error: no cards remaining!");
+                console.log(drawRes.data.remaining);
             }
             const card = drawRes.data.cards[0];
             setDraw(apiRes => [...apiRes,
@@ -40,19 +41,24 @@ const Deck = () => {
                 }
             ]);
         }
+        // drawCard();
     });
     useEffect(() => {
-        console.log("DRAWING CARD...");
+        if (autoDraw && !timerId.current) {
+            console.log("DRAWING CARD...");
         timerId.current = setInterval(() => {
             setCard(c => c + 1);
+            // await drawCard();
         }, 1000);
+    }
         return function clearTimer() {
             console.log("Unmount deck", timerId.current);
             clearInterval(timerId.current);
         };
-    }, [timerId, deck]);
+ 
+    }, [deck,  setAutoDraw, autoDraw]);
 
-    const toggle = (e) => {
+    const toggle = () => {
         setAutoDraw(isAuto => !isAuto);
     };
     const cards = draw.map(c => (
@@ -61,14 +67,15 @@ const Deck = () => {
 
 
     return (
-        <div>
-            <button onClick={toggle}>
-                {autoDraw ? "Start Drawing" : "Stop Drawing"}
+        <div className="Deck">
+        <h1 className="section-header">Deck of Cards</h1>
+            <button className="draw-card" onClick={toggle}>
+                {autoDraw ? "Stop drawing" : "Start drawing"} 
             </button>
+          {/* {deck ? autoDraw : null} */}
+          <div className="Deck-cardarea">{cards}</div>
         </div>
-    )
-
-
+    );
 }
 
 export default Deck;
